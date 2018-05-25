28a7365882865d4cd64aae2c81eab0fd59f1b54e
beefeater <ruihanf2016@gmail.com>
2018-05-25 00:17:26 -0400

    Adding oj client

---
+++ oj-client/src/app/mock-problems.ts
@@ -0,0 +1,29 @@
+import { Problem } from "./models/problem.model";
+
+export const PROBLEMS: Problem[] = [
+  {id: 1,
+    name: "Two Sum",
+    desc: "Given an array of integers, find two numbers such that they add up to a specific target number. The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are NOT zero-based.",
+    difficulty: "easy"
+  },
+  {id: 2,
+    name: "Three Sum",
+    desc: "Given an array of integers, find two numbers such that they add up to a specific target number. The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are NOT zero-based.",
+    difficulty: "medium"
+  },
+  {id: 3,
+    name: "Four Sum",
+    desc: "Given an array of integers, find two numbers such that they add up to a specific target number. The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are NOT zero-based.",
+    difficulty: "medium"
+  },
+  {id: 4,
+    name: "kSum",
+    desc: "Given an array of integers, find two numbers such that they add up to a specific target number. The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are NOT zero-based.",
+    difficulty: "hard"
+  },
+  {id: 5,
+    name: "kkSum",
+    desc: "Given an array of integers, find two numbers such that they add up to a specific target number. The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are NOT zero-based.",
+    difficulty: "super"
+  }
+]
